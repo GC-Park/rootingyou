a5d6cf857dc643a737365c98d3efb4fd1f89ec11
GC-Park <kuyt3122@naver.com>
2022-06-25 15:43:50 +0900

    make it better

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+const ejs = require('ejs')
+
+const app = new express()
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res)=>{
+  res.render('index');
+})
+app.get('/about', (req, res)=>{
+  res.render('about');
+})
+app.get('/contact', (req, res)=>{
+  res.render('contact');
+})
+app.get('/post', (req, res)=>{
+  res.render('post');
+})
+
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+    console.log('App listening on port 8000')
+})
